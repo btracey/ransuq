492f0bd7202cfe1a8672f27e4431001e54d2c70a
btracey <bdtinternet@gmail.com>
2014-06-30 12:40:15 -0700

    Fixed batch script output

--- mainscript/cases/batchscript.sh
+++ mainscript/cases/batchscript.sh
@@ -2,4 +2,4 @@
 #SBATCH --cpus-per-task=12
 #SBATCH --ntasks=1
 NAME=ransuq_$1
-srun ransuqquick -location=cluster -j=$NAME --error=/ADL/btracey/mygo/batch/$NAME_error.txt --output=/ADL/btracey/mygo/batch/$NAME_output.txt
+srun mainscript -location=cluster -j=$NAME --error=$NAME_error.txt --output=$NAME_output.txt
