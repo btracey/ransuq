441f012579654e8cd03050d8e5910a9047c1b3fc
btracey <bdtinternet@gmail.com>
2014-09-15 18:48:46 -0700

    Updated batch script from zion

--- mainscript/batchscript.sh
+++ mainscript/batchscript.sh
@@ -2,11 +2,5 @@
 #SBATCH --job-name="ransuq"
 #SBATCH --cpus-per-task=12
 #SBATCH --ntasks=1
-
 echo $1
-
 srun mainscript $1 -location=cluster
-
-
-#aoseuth SBATCH --output="ransuq_slurm_$NAME.out"
-#oeuast SBATCH --error="panic.err"
